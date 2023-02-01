0f47ba809b6fbbfdff139f789f34dd21f9976e06
lihaixin <lihaixin@15099.net>
2023-02-01 19:39:51 +0800

    Update templateJson.go

--- service/core/v2ray/templateJson.go
+++ service/core/v2ray/templateJson.go
@@ -8,7 +8,7 @@ const TemplateJson = `
             "listen": "0.0.0.0",
             "protocol": "socks",
             "sniffing": {
-                "enabled": false,
+                "enabled": true,
                 "destOverride": [
                     "http",
                     "tls"
@@ -41,7 +41,7 @@ const TemplateJson = `
             "listen": "0.0.0.0",
             "protocol": "socks",
             "sniffing": {
-                "enabled": false,
+                "enabled": true,
                 "destOverride": [
                     "http",
                     "tls"
